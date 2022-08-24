85f662bcf6d54376862fbd0e259add6f8b29d0f5
Nurul Islam <nurulbdpersonal@gmail.com>
2022-08-24 12:37:31 +0600

    script.js added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// import moment from 'moment';
+// moment().format();
+
+const date = new Date();
+const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+
+// const today = new Date();
+// const year = today.getFullYear();
+// <small>Copyright © {year}  All Rights Reserved  home-food.catering.com </small>
+
+// Time
+// moment().format('LTS');
+// // day 
+// moment().format('dddd');
+// // date
+// moment().format("MMM Do YY")
+// 
